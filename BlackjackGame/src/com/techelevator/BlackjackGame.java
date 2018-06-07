e624517a22902874935190e788e835bbb2a82b95
Kevin Sohngen <sohngen.5@osu.edu>
2018-06-07 10:57:46 -0400

    It works

--- BlackjackGame/src/com/techelevator/BlackjackGame.java
+++ BlackjackGame/src/com/techelevator/BlackjackGame.java
@@ -90,15 +90,11 @@ public class BlackjackGame {
 				}
 			}
 		}
-		if (playerAces > 0) {
-			System.out.println("Your Hand: " + removedCards.get(0) + " " + removedCards.get(2) + " for a score of "
-					+ playerCount + " or " + playerCountWithAces);
-			System.out.println("Dealer shows: " + removedCards.get(3));
-		} else {
+
 			System.out.println(
 					"Your Hand: " + removedCards.get(0) + " " + removedCards.get(2) + " for a score of " + playerCount);
 			System.out.println("Dealer shows: " + removedCards.get(3));
-		}
+		
 
 		/*
 		 * if (dealerAces > 0) { System.out.println("Dealer Hand: " +
@@ -114,28 +110,28 @@ public class BlackjackGame {
 		String hit = "Hit";
 		String quit = "";
 		String action = "";
-		while (!(quit.equals("q"))) {
+		while (!(quit.equals("N"))) {   // START OF GAME, HIT 'q' TO QUIT
 
-			if (quit.equals("q")) {
+			if (quit.equals("N")) {
 				System.out.println("User has quit Game");
 				break;
 			}
 
 			while (playerCount < 22) {
+				
+				if (playerCount == 21) {
+					System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
+					System.out.println("Player Wins");
+					break;
+				}
+				
 				System.out.println("Hit or Stand?");
 				action = input.nextLine();
 
 				if (action.equals(stand)) {
 					break;
-				} else if (action.equals("q")) {
-					quit = "q";
-					break;
-				} else if (playerCount == 21) {
-					System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
-					System.out.println("Player Wins");
-					break;
-				}
-
+				} 
+				
 				if (action.equals(hit)) {
 					double random1To52 = Math.random() * cardsLeft; // deal mew card
 					cardsLeft--;
@@ -144,24 +140,27 @@ public class BlackjackGame {
 							cardsAndValues.get(deckCardArrayList.get(cardNumber)));
 					playerCount += cardsAndValues.get(deckCardArrayList.get(cardNumber));
 
-					if (deckValueArrayList.get(cardNumber) == 11) { // check total with Aces
-						if (playerCount > 21) { // add ACEcounter
+					if (playerCount > 21) {
+						if (deckCardArrayList.get(cardNumber).substring(0, 1).equals("A")) {
 							playerCount = playerCount - 10;
+							playerAces--;
 						}
-					} else if (playerCount > 21) {
-						System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
-						System.out.println("Bust."); // player busts and loop quits
-						quit = "q";
-						break;
+						else {
+							System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
+							System.out.println("Bust."); // player busts and loop quits
+							break;							
+						}
 					} else if (playerHand.size() == 7) {
 						System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
 						System.out.println("Player Wins"); // player wins and loop quits
-						quit = "q";
 						break;
-					} else if (playerCount == 21) {
+					} else if (playerCount == 21 && playerHand.size() == 2) {
 						System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
-						System.out.println("Player Wins"); // player wins and loop quits
-						quit = "q";
+						System.out.println("Player Wins");
+						break;
+					}
+					else if (playerCount == 21) {
+						System.out.println("Your Hand: " + playerHand.keySet() + " for a score of " + playerCount);
 						break;
 					}
 					removedCards.add(deckCardArrayList.get(cardNumber)); // remove card from deck
@@ -176,7 +175,7 @@ public class BlackjackGame {
 				}
 			}
 
-			if (action.equals(stand)) {
+			if (action.equals(stand) || (playerCount == 21 && playerHand.size() != 2)) {
 				System.out.println("Dealer Flips");
 				System.out.println("Dealer Hand: " + dealerHand.keySet() + " for a score of " + dealerCount);
 
@@ -199,20 +198,81 @@ public class BlackjackGame {
 						System.out.println("Dealer stands");
 					}
 				}
-				if (playerCount > dealerCount) {
+				if (dealerCount > 21) {
+					
 					System.out.println("Player Wins");
-					quit = "q";
-				} else if (dealerCount > playerCount) {
+				}
+				if (playerCount > dealerCount && dealerCount < 22) {
+					System.out.println("Player Wins");
+					
+				} else if (dealerCount > playerCount  && dealerCount < 22) {
 					System.out.println("Dealer Wins");
-					quit = "q";
-				} else if (dealerCount == playerCount) {
-					System.out.println("Push and Play Again");
-					break;
+
+				} else if (dealerCount == playerCount  && dealerCount < 22) {
+					System.out.println("Push");
 				}
 			}
+			
+			System.out.println("Play again? Y/N");
+			quit = input.nextLine();
+			if (quit.equals("Y")) {
+				deckCardArrayList.addAll(removedCards);
+				removedCards.clear();
+				dealerHand.clear();
+				playerHand.clear();
+				dealerCount = 0;
+				dealerAces = 0;
+				playerCount = 0;
+				playerAces = 0;
+				cardsLeft = 52;
+				for (i = 0; i < 4; i++) {
+					double random1To52 = Math.random() * cardsLeft; // create random integer 1 to 52
+					cardsLeft--;
+					int cardNumber = (int) random1To52; // set card number equal to random integer 1 to 52
 
+					if (i % 2 == 0) { // populate players hand and value
+						if (deckCardArrayList.get(cardNumber).charAt(0) == 'A') {
+							playerAces++;
+						}
+						playerHand.put(deckCardArrayList.get(cardNumber),
+								cardsAndValues.get(deckCardArrayList.get(cardNumber)));
+						playerCount += cardsAndValues.get(deckCardArrayList.get(cardNumber));
+						removedCards.add(deckCardArrayList.get(cardNumber));
+						deckCardArrayList.remove(cardNumber);
+					} else { // populate dealers hand and value
+						if (deckCardArrayList.get(cardNumber).charAt(0) == 'A') {
+							dealerAces++;
+						}
+						dealerHand.put(deckCardArrayList.get(cardNumber),
+								cardsAndValues.get(deckCardArrayList.get(cardNumber)));
+						dealerCount += cardsAndValues.get(deckCardArrayList.get(cardNumber));
+						removedCards.add(deckCardArrayList.get(cardNumber));
+						deckCardArrayList.remove(cardNumber);
+					}
+
+				}
+				for (int j = 0; j < 4; j++) {
+					if (j == 0 || j == 2) {
+						if (cardsAndValues.get(removedCards.get(j)) == 11) {
+							playerCountWithAces = playerCount - (playerAces * 10);
+						}
+					} else {
+						if (cardsAndValues.get(removedCards.get(j)) == 11) {
+							dealerCountWithAces = dealerCount - (dealerAces * 10);
+						}
+					}
+				}
+				if (playerAces > 0) {
+					System.out.println("Your Hand: " + removedCards.get(0) + " " + removedCards.get(2) + " for a score of "
+							+ playerCount + " or " + playerCountWithAces);
+					System.out.println("Dealer shows: " + removedCards.get(3));
+				} else {
+					System.out.println(
+							"Your Hand: " + removedCards.get(0) + " " + removedCards.get(2) + " for a score of " + playerCount);
+					System.out.println("Dealer shows: " + removedCards.get(3));
+				}
+			}
 		}
-
 	}
 }
 
